8ceb45f03f4484f89d6264d960ed1cb0e83f0f28
Oscar Avelar <oavelar@applaudostudios.com>
2021-10-08 07:26:26 -0600

    Fix issue with modal not showing current send data, add localStorage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import Inicio from './components/Inicio';
 import 'antd/dist/antd.css';
 
 function App() {
-  return (
-    <Inicio/>
-  );
+  return <Inicio />;
 }
 
 export default App;
--- src/components/EditModal.jsx
+++ src/components/EditModal.jsx
@@ -1,1 +1,28 @@
-import React from 'react';
+import React, {useState} from "react";
+import { Modal, Form, Input, Select } from "antd";
+
+const EditModal = ({modalState = false, dataToUpdate = {}, closeModal, key}) => {
+  const {customerType, printerModel} = dataToUpdate;
+  return (
+    <Modal
+      key={key}
+      title="Basic Modal"
+      visible={modalState}
+      onCancel={() => closeModal()}
+    >
+      <>
+        <Form.Item label="Printer Model" name="printerModel">
+          <Input defaultValue={printerModel}/>
+        </Form.Item>
+        <Form.Item label="Customer type" name="customerType">
+          <Select defaultValue={customerType}>
+            <Select.Option value="opcion1">opcion1</Select.Option>
+            <Select.Option value="opcion2">opcion2</Select.Option>
+          </Select>
+        </Form.Item>
+      </>
+    </Modal>
+  );
+}
+
+export default EditModal;
